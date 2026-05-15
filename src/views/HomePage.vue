36a039e3b1e31e5db8a31fe002eeeeef8983e620
3nrique-Resendiz <quiqueyael6@gmail.com>
2026-05-14 20:36:23 -0600

    Update HomePage.vue

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -7,7 +7,7 @@
     </ion-header>
 
     <ion-content class="ion-padding">
-      <div class="display">
+      <div class="display" translate="no">
         <div class="operacion">{{ buffer }}</div>
         <h1>{{ resultado }}</h1>
       </div>
@@ -52,16 +52,23 @@ const presionar = (boton: string) => {
   } else if (boton === '=') {
     try {
       // Lógica simple de reemplazo para evaluar
-      let expresion = resultado.value.replace(/X/g, '*');
+      let expresion = resultado.value.replace(/X/g, '*')
+                                  .replace(/sin/g, 'Math.sin')
+                                  .replace(/cos/g, 'Math.cos')
+                                  .replace(/sqrt/g, 'Math.sqrt');
       resultado.value = eval(expresion).toString();
     } catch {
       resultado.value = 'Error';
     }
+  } else if (['sin', 'cos', 'sqrt'].includes(boton)) {
+    // Esto hace que al picar 'sin' escriba 'sin(' para que el usuario ponga el número
+    if (resultado.value === '0') resultado.value = boton + '(';
+    else resultado.value += boton + '(';
   } else {
     if (resultado.value === '0') resultado.value = boton;
     else resultado.value += boton;
   }
-};
+};  
 
 const esOperador = (b: string) => ['/', 'X', '-', '+', '=', 'sin', 'cos', 'sqrt', 'C'].includes(b);
 </script>
